a98b8b45db6c3cc7c931e50f8299f9db17b5dc1d
Hanna Kozmina <kozminahanna@gmail.com>
2025-10-25 19:49:37 +0300

    Update test.e2e2.js: WDIO test v.02

--- test/specs/test.e2e2.js
+++ test/specs/test.e2e2.js
@@ -1,17 +1,21 @@
-import LoginPage from "./../pages/main.page.js"
+import LoginPage from "./../pages/main.page.js";
 
 describe("Webdriverio main page", () => {
+  it("should show addValue command", async () => {
+    await browser.url("https://the-internet.herokuapp.com/login");
 
-    it("should show addValue command", async () => {
-        await browser.url('https://the-internet.herokuapp.com/login');
+    await LoginPage.setUsernameInput(123);
+    await browser.pause(2000);
 
-        await LoginPage.setUsernameInput(123);
-        await browser.pause(2000);
+    await LoginPage.username.addValue("hello");
+    await browser.pause(2000);
 
-        await LoginPage.username.addValue("hello");
-        await browser.pause(2000);
+    await LoginPage.setPasswordInput("superpassword!");
+    await browser.pause(2000);
 
-        await expect(LoginPage.username).toHaveValue("123hello");
-    });
+    await LoginPage.clickOnLoginBtn();
+    await browser.pause(2000);
 
-})
+    await expect(LoginPage.username).toHaveValue("123hello");
+  });
+});
